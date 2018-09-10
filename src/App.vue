8c4f963a7fe35de34a332e08e0b7493a94ae6b04
guitarbien <guitarbien@gmail.com>
2018-09-10 14:29:50 +0800

    03 Wrapping External Libraries as Vue Components
    在 App.vue 加入第三方 date picker 套件

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,34 @@
   <div class="min-h-screen bg-grey-darker p-8">
     <div class="max-w-sm mx-auto">
       <user-settings-form></user-settings-form>
+
+      <div class="card">
+        <label class="block">
+          <span class="form-label mb-2">Select a date</span>
+          <input class="form-input" type="text" id="datePicker" placeholder="YYYY-MM-DD">
+        </label>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
+import Pikaday from 'pikaday';
 import UserSettingsForm from './components/UserSettingsForm.vue';
 
 export default {
   components: {
     UserSettingsForm,
   },
+  mounted() {
+    const picker = new Pikaday({
+      field: document.getElementById('datePicker'),
+      format: 'YYYY-MM-DD',
+    });
+  },
 };
 </script>
 
+<style src="./assets/css/pikaday.css"></style>
 <style src="./assets/css/app.css"></style>
