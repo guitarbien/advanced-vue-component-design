9933bff4dbb2e194ef5d3a5327b38fd7b7d534d2
guitarbien <guitarbien@gmail.com>
2018-09-10 11:44:17 +0800

    01 Building Controlled Components
    init a basic feature

---
+++ src/components/UserSettingsForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <form class="card" @submit.prevent="submit">
+    <h1 class="text-2xl font-bold mb-6">Your Settings</h1>
+    <label class="block mb-6">
+      <span class="form-label mb-2">Email Address</span>
+      <input v-model="email" class="form-input" placeholder="you@example.com">
+    </label>
+    <div class="mb-8">
+      <span class="form-label mb-2">Newsletter</span>
+      <div class="flex justify-between items-center">
+        <p class="mr-4">Send you occasional news and updates.</p>
+        <span class="toggle flex-no-shrink" role="checkbox" tabindex="0"
+              :aria-checked="receiveNewsletter"
+              @click="toggleNewsletter"
+              @keydown.space.prevent="toggleNewsletter"
+        ></span>
+      </div>
+    </div>
+    <div class="text-right">
+      <button type="submit" class="btn btn-blue">Update Settings</button>
+    </div>
+  </form>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      email: 'jane@example.com',
+      receiveNewsletter: false,
+    };
+  },
+  methods: {
+    submit() {
+      console.log('Submitting preferences...', {
+        email: this.email,
+        receiveNewsletter: this.receiveNewsletter,
+      });
+    },
+    toggleNewsletter() {
+      this.receiveNewsletter = !this.receiveNewsletter;
+    },
+  },
+};
+</script>
