615bfc538596378ac8b5d331d2647ff7d342f07e
guitarbien <guitarbien@gmail.com>
2018-09-29 17:02:03 +0800

    AnnouncementModal 使用 <modal-dialog> 並移除重複程式碼

--- src/components/AnnouncementModal.vue
+++ src/components/AnnouncementModal.vue
@@ -1,53 +1,29 @@
 <template>
-  <div class="modal-backdrop" v-show="show">
-    <div class="modal">
-      <h1 class="text-center text-2xl font-bold mb-4">
-        Exciting new features are here!
-      </h1>
-      <p class="text-center text-grey-darker mb-6">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ut eligendi quod tempore totam explicabo sit consectetur architecto? Tempora, repellat est rem ut esse ab officia saepe ratione tempore. Obcaecati.
-      </p>
-      <div class="text-center">
-        <button @click="dismiss" type="button" class="btn btn-blue">
-          Dismiss
-        </button>
-      </div>
+  <modal-dialog>
+    <h1 class="text-center text-2xl font-bold mb-4">
+      Exciting new features are here!
+    </h1>
+    <p class="text-center text-grey-darker mb-6">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ut eligendi quod tempore totam explicabo sit consectetur architecto? Tempora, repellat est rem ut esse ab officia saepe ratione tempore. Obcaecati.
+    </p>
+    <div class="text-center">
+      <button @click="dismiss" type="button" class="btn btn-blue">
+        Dismiss
+      </button>
     </div>
-  </div>
+  </modal-dialog>
 </template>
 
 <script>
+import ModalDialog from './ModalDialog.vue';
+
 export default {
+  components: {
+    ModalDialog,
+  },
   props: {
     show: { required: true },
     preventBackgroundScrolling: { default: true },
-  },
-  watch: {
-    show: {
-      immediate: true,
-      handler() {
-        if (this.preventBackgroundScrolling) {
-          if (this.show) {
-            document.body.style.setProperty('overflow', 'hidden');
-          } else {
-            document.body.style.removeProperty('overflow');
-          }
-        }
-      },
-    },
-  },
-  created() {
-    const escapeHandler = (e) => {
-      if (e.key === 'Escape' && this.show) {
-        this.dismiss();
-      }
-    };
-
-    document.addEventListener('keydown', escapeHandler);
-
-    this.$once('hook:destroyed', () => {
-      document.addEventListener('keydown', escapeHandler);
-    });
   },
   methods: {
     dismiss() {
