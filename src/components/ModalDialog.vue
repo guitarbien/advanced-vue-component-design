cac19bad0cc3f398fd6fda57944a73e3ce86954c
guitarbien <guitarbien@gmail.com>
2018-09-29 16:54:08 +0800

    有鑑於 AnnouncementModal 和 ConfirmDeleteModel 功能過於相似，將重複的程式碼抽出到 ModalDialog component 內

---
+++ src/components/ModalDialog.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="modal-backdrop" v-show="show">
+    <div class="modal">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalDialog",
+  props: ['show'],
+  watch: {
+    show: {
+      immediate: true,
+      handler: (show) => {
+        if (show) {
+          document.body.style.setProperty('overflow', 'hidden');
+        } else {
+          document.body.style.removeProperty('overflow');
+        }
+      },
+    },
+  },
+  methods: {
+    cancel() {
+      this.$emit('close');
+    },
+  },
+  created() {
+    const escapeHandler = (e) => {
+      if (e.key === 'Escape' && this.show) {
+        this.cancel();
+      }
+    };
+    document.addEventListener('keydown', escapeHandler);
+    this.$once('hook:destroyed', () => {
+      document.removeEventListener('keydown', escapeHandler);
+    });
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
