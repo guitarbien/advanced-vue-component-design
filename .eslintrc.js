e1b16562e6f188f3243fb742160d1c06ee2f7d7e
guitarbien <guitarbien@gmail.com>
2018-10-04 21:18:01 +0800

    (調整 eslint rule)

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': ["error", { "code": 200 }],
+    'max-len': ["error", { "code": 500 }],
   },
   parserOptions: {
     parser: 'babel-eslint',
