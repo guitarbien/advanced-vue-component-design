937a4031710cca2d9dc3c21ac7c3c6c790cf0115
guitarbien <guitarbien@gmail.com>
2018-10-07 11:38:34 +0800

    把 component 的內容放到外部 slot 的位置，但這樣還不會顯示

--- src/App.vue
+++ src/App.vue
@@ -85,7 +85,23 @@
       </div>
 
       <!-- 16 -->
-      <new-contact-list></new-contact-list>
+      <new-contact-list>
+        <div class="card">
+          <h1 class="text-2xl font-bold mb-6">Your Contacts</h1>
+          <div>
+            <div v-for="contact in contacts" :key="contact.id" class="flex items-center spaced-y-6">
+              <img :src="contact.picture.medium" class="h-12 w-12 rounded-full block mr-2" alt="">
+              <div>
+                <div class="font-bold">
+                  {{ contact.name.first }} {{ contact.name.last }}
+                </div>
+                <div class="text-grey-dark">
+                  {{ contact.email }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div></new-contact-list>
     </div>
   </div>
 </template>
