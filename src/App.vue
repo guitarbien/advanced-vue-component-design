55b6b9fe893340aaee5d55b8dfcb1905661727a7
guitarbien <guitarbien@gmail.com>
2018-10-07 12:01:37 +0800

    destructing 時透過 alias 保持變數名稱的意義

--- src/App.vue
+++ src/App.vue
@@ -86,10 +86,10 @@
 
       <!-- 16 -->
       <fetch-json url="/contact.json">
-        <div class="card" slot-scope="{ json }">
+        <div class="card" slot-scope="{ json: contacts }">
           <h1 class="text-2xl font-bold mb-6">Your Contacts</h1>
           <div>
-            <div v-for="contact in json" :key="contact.id" class="flex items-center spaced-y-6">
+            <div v-for="contact in contacts" :key="contact.id" class="flex items-center spaced-y-6">
               <img :src="contact.picture.medium" class="h-12 w-12 rounded-full block mr-2" alt="">
               <div>
                 <div class="font-bold">
