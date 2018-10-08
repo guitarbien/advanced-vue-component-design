e8bd25f3323314d85c4b7f0b1642590c569ff17c
guitarbien <guitarbien@gmail.com>
2018-10-08 11:36:54 +0800

    component 參考原先的 <tag-input> 使用 model 將 value prop 和 input event 改為 tags prop 和 update event

--- src/components/RenderlessTagInput.vue
+++ src/components/RenderlessTagInput.vue
@@ -1,6 +1,11 @@
 <script>
 export default {
   name: 'RenderlessTagInput',
+  model: {
+    prop: 'tags',
+    event: 'update',
+  },
+  props: ['tags'],
   render() {
     return this.$scopedSlots.default({
 
