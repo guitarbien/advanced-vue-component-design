39695b4af69740202133428c9af2ae47ebd701e0
guitarbien <guitarbien@gmail.com>
2018-10-08 13:48:11 +0800

    避免外部需要知道過多細節，將 bind 和 event listener 包裝成兩個由 scoped slot 提供的物件，直接使用 v-bind="" 和 v-on="" 同時宣告完畢

--- src/App.vue
+++ src/App.vue
@@ -115,7 +115,7 @@
     <div class="max-w-sm mx-auto card mt-8">
       <label class="form-label mb-2">Renderless Tag Input</label>
       <renderless-tag-input v-model="tags">
-        <div class="tag-input" slot-scope="{ tags, removeTag }">
+        <div class="tag-input" slot-scope="{ tags, removeTag, inputProps, inputEvents }">
           <span v-for="tag in tags" :key="tag" class="tag-input-tag">
             <span>{{ tag }}</span>
             <button type="button" class="tag-input-remove"
@@ -123,10 +123,8 @@
             >&times;</button>
           </span>
           <input class="tag-input-text" placeholder="Add tag..."
-                 :value="input"
-                 @input="(e) => input = e.target.value"
-                 @keydown.backspace="handleBackspace"
-                 @keydown.enter.prevent="addTag"
+                 v-bind="inputProps"
+                 v-on="inputEvents"
           >
         </div>
       </renderless-tag-input>
--- src/components/RenderlessTagInput.vue
+++ src/components/RenderlessTagInput.vue
@@ -15,6 +15,21 @@ export default {
     return this.$scopedSlots.default({
       tags: this.tags,
       removeTag: this.removeTag,
+      inputProps: {
+        value: this.input,
+      },
+      inputEvents: {
+        input: (e) => this.input = e.target.value,
+        keydown: (e) => {
+          if (e.key === 'Backspace') {
+            this.handleBackspace();
+          }
+          if (e.key === 'Enter') {
+            e.preventDefault();
+            this.addTag();
+          }
+        },
+      },
     });
   },
 };
