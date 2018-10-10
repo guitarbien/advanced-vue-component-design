39c752a6b62dd0bee30eeed98d89a7324d7c9429
guitarbien <guitarbien@gmail.com>
2018-10-10 21:42:15 +0800

    將原本內容搬移至新增的 ProfileCard 內

--- src/App.vue
+++ src/App.vue
@@ -131,34 +131,10 @@
     <!-- 25 -->
     <div class="min-h-screen bg-grey-darker p-4">
       <div style="width: 420px" class="mx-auto border-l border-r border-dashed border-grey-dark py-4 mb-4">
-        <with-dimensions>
-          <div slot-scope="{ width }" class="profile-card" :class="{ 'profile-card--horizontal': width >= 400 }">
-            <img class="profile-card-image" src="/img/toby.jpg" alt="">
-            <div class="profile-card-meta">
-              <div class="mb-4">
-                <p class="text-xl leading-tight">Toby Flenderson</p>
-                <p class="text-sm leading-tight text-grey-dark">Human Resources at Dunder Mifflin</p>
-              </div>
-              <div>
-                <button class="btn btn-sm btn-pill btn-indigo-outline">Message</button>
-              </div>
-            </div>
-          </div>
-        </with-dimensions>
+        <profile-card></profile-card>
       </div>
       <div style="width: 300px" class="border-l border-r border-dashed border-grey-dark py-4 mx-auto">
-        <div class="profile-card">
-          <img class="profile-card-image" src="/img/toby.jpg" alt="">
-          <div class="profile-card-meta">
-            <div class="mb-4">
-              <p class="text-xl leading-tight">Toby Flenderson</p>
-              <p class="text-sm leading-tight text-grey-dark">Human Resources at Dunder Mifflin</p>
-            </div>
-            <div>
-              <button class="btn btn-sm btn-pill btn-indigo-outline">Message</button>
-            </div>
-          </div>
-        </div>
+        <profile-card></profile-card>
       </div>
     </div>
 
@@ -166,6 +142,7 @@
 </template>
 
 <script>
+import ProfileCard from './components/ProfileCard.vue';
 import WithDimensions from './components/WithDimensions.vue';
 import StackedTagInput from './components/StackedTagInput.vue';
 import InlineTagInput from './components/InlineTagInput.vue';
@@ -183,6 +160,7 @@ import MediaCard from './components/MediaCard.vue';
 
 export default {
   components: {
+    ProfileCard,
     WithDimensions,
     StackedTagInput,
     InlineTagInput,
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <with-dimensions>
+    <div slot-scope="{ width }" class="profile-card" :class="{ 'profile-card--horizontal': width >= 400 }">
+      <img class="profile-card-image" src="/img/toby.jpg" alt="">
+      <div class="profile-card-meta">
+        <div class="mb-4">
+          <p class="text-xl leading-tight">Toby Flenderson</p>
+          <p class="text-sm leading-tight text-grey-dark">Human Resources at Dunder Mifflin</p>
+        </div>
+        <div>
+          <button class="btn btn-sm btn-pill btn-indigo-outline">Message</button>
+        </div>
+      </div>
+    </div>
+  </with-dimensions>
+</template>
+
+<script>
+import WithDimensions from './WithDimensions.vue';
+
+export default {
+  name: 'ProfileCard',
+  components: {
+    WithDimensions,
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
