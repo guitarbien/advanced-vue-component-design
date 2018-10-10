2f76a072d907b6c657392587eb77ec1d72d8c6d7
guitarbien <guitarbien@gmail.com>
2018-10-10 21:27:58 +0800

    新增 WithDimensions component 使用第三方 element-resize-detector 套件來偵測 element 的長寬變化

---
+++ src/components/WithDimensions.vue
@@ -0,0 +1,27 @@
+<script>
+import elementResizeDetectorMaker from 'element-resize-detector';
+
+const erd = elementResizeDetectorMaker({ strategy: 'scroll' });
+
+export default {
+  name: 'WithDimensions',
+  data() {
+    return {
+      width: null,
+      height: null,
+    };
+  },
+  mounted() {
+    erd.listenTo(this.$el, (el) => {
+      this.width = el.offsetWidth;
+      this.height = el.offsetHeight;
+    });
+  },
+  render() {
+    return this.$scopedSlots.default({
+      width: this.width,
+      height: this.height,
+    });
+  },
+};
+</script>
