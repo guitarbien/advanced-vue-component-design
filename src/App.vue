c8bbf356cd2ee10e3ca44aa834af0e4003ee4ef1
guitarbien <guitarbien@gmail.com>
2018-10-10 21:10:24 +0800

    25 Element Queries as a Data Provider Component

--- src/App.vue
+++ src/App.vue
@@ -127,6 +127,39 @@
       <label class="form-label mb-2">Stacked Tag Input</label>
       <stacked-tag-input v-model="tags"></stacked-tag-input>
     </div>
+
+    <!-- 25 -->
+    <div class="min-h-screen bg-grey-darker p-4">
+      <div style="width: 420px" class="mx-auto border-l border-r border-dashed border-grey-dark py-4 mb-4">
+        <div class="profile-card profile-card--horizontal">
+          <img class="profile-card-image" src="/img/toby.jpg" alt="">
+          <div class="profile-card-meta">
+            <div class="mb-4">
+              <p class="text-xl leading-tight">Toby Flenderson</p>
+              <p class="text-sm leading-tight text-grey-dark">Human Resources at Dunder Mifflin</p>
+            </div>
+            <div>
+              <button class="btn btn-sm btn-pill btn-indigo-outline">Message</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style="width: 300px" class="border-l border-r border-dashed border-grey-dark py-4 mx-auto">
+        <div class="profile-card">
+          <img class="profile-card-image" src="/img/toby.jpg" alt="">
+          <div class="profile-card-meta">
+            <div class="mb-4">
+              <p class="text-xl leading-tight">Toby Flenderson</p>
+              <p class="text-sm leading-tight text-grey-dark">Human Resources at Dunder Mifflin</p>
+            </div>
+            <div>
+              <button class="btn btn-sm btn-pill btn-indigo-outline">Message</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
