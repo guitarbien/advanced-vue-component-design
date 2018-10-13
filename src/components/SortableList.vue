7a27bdf9ef47db40580769ff7b66982b6bccb261
guitarbien <guitarbien@gmail.com>
2018-10-13 19:55:10 +0800

    使用 @shopify/draggable 的 sortable 效果

--- src/components/SortableList.vue
+++ src/components/SortableList.vue
@@ -1,6 +1,17 @@
 <script>
+import { Sortable } from '@shopify/draggable';
+
 export default {
   name: 'SortableList',
+  mounted() {
+    new Sortable(document.querySelectorAll('.contact-list'), {
+      draggable: '.contact-list-item',
+      handle: '.contact-list-handle',
+      mirror: {
+        constrainDimensions: true,
+      },
+    });
+  },
   render() {
     return this.$slots.default[0];
   },
