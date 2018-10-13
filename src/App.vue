4ad1efa8d7838b35c7240217d3f9827f18f85229
guitarbien <guitarbien@gmail.com>
2018-10-13 19:04:39 +0800

    27 Building a Compound Sortable List Component

--- src/App.vue
+++ src/App.vue
@@ -139,7 +139,7 @@
     </div>
 
     <!-- 26 -->
-    <div class="min-h-screen bg-grey-darker p-8">
+    <div class="min-h-0 bg-grey-darker p-8">
       <div class="max-w-sm mx-auto">
         <accordion-list>
           <accordion-item :item-id="1">
@@ -170,6 +170,30 @@
       </div>
     </div>
 
+    <!-- 27 -->
+    <div class="min-h-0 bg-grey-darker p-8">
+      <div class="max-w-sm mx-auto">
+        <div class="card">
+          <h1 class="text-2xl font-bold mb-6">Your Contacts</h1>
+            <div class="contact-list">
+              <div class="contact-list-item" v-for="contact in contacts" :key="contact.id">
+                <div class="contact-list-contact">
+                  <img :src="contact.avatar" class="contact-list-avatar" alt="">
+                  <div>
+                    <div class="contact-list-name">
+                      {{ contact.name }}
+                    </div>
+                    <div class="contact-list-email">
+                      {{ contact.email }}
+                    </div>
+                  </div>
+                </div>
+                <svg class="contact-list-handle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path fill-rule="evenodd" d="M14 4h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1zM8 4h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1zm6 6h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1zm-6 0h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1zm6 6h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1zm-6 0h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1z"/></svg>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -219,6 +243,38 @@ export default {
       modalOpen: true,
       accountId: 7,
       tags: ['awesome', 'excellent', 'amazing'],
+      contacts: [
+        {
+          id: 1,
+          name: 'Alfreda Ferreira',
+          email: 'alfreda.ferreira@example.com',
+          avatar: 'https://randomuser.me/api/portraits/med/women/6.jpg',
+        },
+        {
+          id: 2,
+          name: 'Leevi Wirtanen',
+          email: 'leevi.wirtanen@example.com',
+          avatar: 'https://randomuser.me/api/portraits/med/men/19.jpg',
+        },
+        {
+          id: 3,
+          name: 'Kaitlin Sutton',
+          email: 'kaitlin.sutton@example.com',
+          avatar: 'https://randomuser.me/api/portraits/med/women/2.jpg',
+        },
+        {
+          id: 4,
+          name: 'Alice Wilson',
+          email: 'alice.wilson@example.com',
+          avatar: 'https://randomuser.me/api/portraits/med/women/62.jpg',
+        },
+        {
+          id: 5,
+          name: 'Etienne Roy',
+          email: 'etienne.roy@example.com',
+          avatar: 'https://randomuser.me/api/portraits/med/men/4.jpg',
+        },
+      ],
     };
   },
 };
