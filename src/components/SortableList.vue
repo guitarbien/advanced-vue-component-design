f21fe0ad4336c9b2612347ff6df1d66bf87388a6
guitarbien <guitarbien@gmail.com>
2018-10-14 19:31:11 +0800

    (fix missing comma)

--- src/components/SortableList.vue
+++ src/components/SortableList.vue
@@ -4,13 +4,13 @@ import { Sortable } from '@shopify/draggable';
 function move(items, oldIndex, newIndex) {
   const itemRemovedArray = [
     ...items.slice(0, oldIndex),
-    ...items.slice(oldIndex + 1, items.length)
+    ...items.slice(oldIndex + 1, items.length),
   ];
 
   return [
     ...itemRemovedArray.slice(0, newIndex),
     items[oldIndex],
-    ...itemRemovedArray.slice(newIndex, itemRemovedArray.length)
+    ...itemRemovedArray.slice(newIndex, itemRemovedArray.length),
   ];
 }
 
