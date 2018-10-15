ab83ae86cba003ee2160ba3378cdcd427a7ca87c
guitarbien <guitarbien@gmail.com>
2018-10-15 13:25:12 +0800

    想讓 input 在 blur 時關閉，新增 rederless component，由外部指定其行為

---
+++ src/components/OnClickOutside.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: 'onClickOutside',
+  props: ['do'],
+  render() {
+    return this.$slots.default[0];
+  },
+};
+</script>
--- src/components/SearchSelect.vue
+++ src/components/SearchSelect.vue
@@ -1,37 +1,44 @@
 <template>
-  <div class="search-select" :class="{ 'is-active' : isOpen}">
-    <button ref="button" type="button" @click="open" class="search-select-input">
-      <span v-if="value !== null">{{ value }}</span>
-      <span v-else class="search-select-placeholder">Select a band...</span>
-    </button>
-    <div v-show="isOpen" class="search-select-dropdown">
-      <input class="search-select-search"
-             v-model="search"
-             ref="search"
-             @keydown.esc="close"
-             @keydown.down="highlightNext"
-             @keydown.up="highlightPrev"
-             @keydown.enter.prevent="selectHighlighted"
-             @keydown.tab.prevent
-      >
-      <ul ref="options" v-show="filteredOptions.length > 0" class="search-select-options">
-        <li class="search-select-option" :class="{ 'is-active': index === highlightedIndex }"
-            v-for="(option, index) in filteredOptions"
-            :key="option"
-            @click="select(option)"
-        >{{ option }}</li>
-      </ul>
-      <div v-show="filteredOptions.length <= 0" class="search-select-empty">
-        No results found for "{{ search }}"
+  <on-click-outside :do="close">
+    <div class="search-select" :class="{ 'is-active' : isOpen}">
+      <button ref="button" type="button" @click="open" class="search-select-input">
+        <span v-if="value !== null">{{ value }}</span>
+        <span v-else class="search-select-placeholder">Select a band...</span>
+      </button>
+      <div v-show="isOpen" class="search-select-dropdown">
+        <input class="search-select-search"
+               v-model="search"
+               ref="search"
+               @keydown.esc="close"
+               @keydown.down="highlightNext"
+               @keydown.up="highlightPrev"
+               @keydown.enter.prevent="selectHighlighted"
+               @keydown.tab.prevent
+        >
+        <ul ref="options" v-show="filteredOptions.length > 0" class="search-select-options">
+          <li class="search-select-option" :class="{ 'is-active': index === highlightedIndex }"
+              v-for="(option, index) in filteredOptions"
+              :key="option"
+              @click="select(option)"
+          >{{ option }}</li>
+        </ul>
+        <div v-show="filteredOptions.length <= 0" class="search-select-empty">
+          No results found for "{{ search }}"
+        </div>
       </div>
     </div>
-  </div>
+  </on-click-outside>
 </template>
 
 <script>
+import OnClickOutside from './OnClickOutside.vue';
+
 export default {
   name: 'SearchSelect',
   props: ['value', 'options', 'filterFunction'],
+  components: {
+    OnClickOutside,
+  },
   data() {
     return {
       isOpen: false,
