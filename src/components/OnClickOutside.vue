9ae14d03de29784f9eb918f60f201e74097b855a
guitarbien <guitarbien@gmail.com>
2018-10-15 13:39:36 +0800

    透過 mounted hook 掛載 click event listener

--- src/components/OnClickOutside.vue
+++ src/components/OnClickOutside.vue
@@ -2,6 +2,19 @@
 export default {
   name: 'onClickOutside',
   props: ['do'],
+  mounted() {
+    const listener = (e) => {
+      if (e.target === this.$el || this.$el.contains(e.target)) {
+        return;
+      }
+
+      this.do();
+    };
+
+    document.addEventListener('click', listener);
+
+    this.$once('hook:beforeDestroy', () => document.removeEventListener('click', listener));
+  },
   render() {
     return this.$slots.default[0];
   },
